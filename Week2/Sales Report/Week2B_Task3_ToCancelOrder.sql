2b11993d267439e5dead096f445d40e32cb4efab
Alwin Antony <162309301+AlwinAntonyThekkeveetile@users.noreply.github.com>
2024-08-30 16:22:28 +0530

    Update Week2B_Task3_ToCancelOrder.sql

--- Week2/Sales Report/Week2B_Task3_ToCancelOrder.sql
+++ Week2/Sales Report/Week2B_Task3_ToCancelOrder.sql
@@ -3,7 +3,8 @@ DELIMITER //
 CREATE PROCEDURE CancelOrder(IN id INT)
 BEGIN
 IF EXISTS(SELECT 1 FROM Orders WHERE OrderID = id)THEN
-DELETE FROM Orders WHERE OrderID = id; SELECT CONCAT("Order ", id, " is cancelled.") AS Confirmation;
+DELETE FROM Orders WHERE OrderID = id; 
+SELECT CONCAT("Order ", id, " is cancelled.") AS Confirmation;
 ELSE
 SELECT CONCAT("Order ", id, " does not exist.") AS Response;
 END IF;END //
